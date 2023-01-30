d992cdc3e257d2db4d6651967bb8d29d0772f0c1
Decio Neto <deciom.neto10@gmail.com>
2023-01-29 22:38:37 -0300

    feat: add hero service

---
+++ src/app/mocks/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "../types/hero";
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Estranho' },
+  { id: 13, name: 'Hulk' },
+  { id: 14, name: 'Capitão América' },
+  { id: 15, name: 'Homem de Ferro' },
+  { id: 16, name: 'Homem Aranha' },
+  { id: 17, name: 'Viuva Negra' },
+  { id: 18, name: 'Pantera Negra' },
+  { id: 19, name: 'Homem Formiga' },
+  { id: 20, name: 'Thor' },
+  { id: 21, name: 'Falcão' }
+];
