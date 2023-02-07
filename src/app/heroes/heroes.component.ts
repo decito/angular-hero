ef3b806128235771e728e6ca85a19c3da19d8e88
Decio Neto <deciom.neto10@gmail.com>
2023-02-07 10:35:04 -0300

    feat: push messages on heroes fetch

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,29 +1,32 @@
 import { Component } from '@angular/core';
+
 import { Hero } from '../types/hero';
-import { HeroService } from '../services/hero.service';
+
+import { HeroService } from '../services/hero/hero.service';
+import { MessageService } from '../services/message/message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-
 export class HeroesComponent {
   heroes: Hero[] = [];
 
   selectedHero?: Hero;
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  };
-
-  getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
-  }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  constructor(private heroService: HeroService) { }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: selected hero id=${ hero.id }`)
+  };
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
+  }
 }
