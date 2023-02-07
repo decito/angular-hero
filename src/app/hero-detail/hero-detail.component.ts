735e30a300e55b0dd3a80d5a2822c5ce6be3bed1
Decio Neto <deciom.neto10@gmail.com>
2023-02-07 16:46:49 -0300

    feat: add routing capability to heroes and details components

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 import { Hero } from '../types/hero';
+
+import { HeroService } from '../services/hero/hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -9,4 +13,24 @@ import { Hero } from '../types/hero';
 })
 export class HeroDetailComponent {
   @Input() hero?: Hero;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  ngOnInit(): void {
+    this.getHero();
+  }
 }
