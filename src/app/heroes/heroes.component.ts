ef4a0e134f9eb6be1f0533da37948560d662c49a
Decio Neto <61292095+decito@users.noreply.github.com>
2023-02-07 16:47:02 -0300

    feat: add routing capability to heroes and details components (#11)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { Hero } from '../types/hero';
 
 import { HeroService } from '../services/hero/hero.service';
-import { MessageService } from '../services/message/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,20 +12,13 @@ import { MessageService } from '../services/message/message.service';
 export class HeroesComponent {
   heroes: Hero[] = [];
 
-  selectedHero?: Hero;
-
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: selected hero id=${ hero.id }`)
-  };
+  constructor(private heroService: HeroService) { }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
 }
